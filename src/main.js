ff8d27244dd0a350b5abd7732098a09a34b5f1bd
adampickeral <adam.pickeral@rackspace.com>
2015-12-18 11:43:15 -0500

    bowling kata using immutable.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,128 @@
+import { List } from 'immutable';
+
+const NUM_FRAMES = 10;
+const NUM_PINS = 10;
+
 class Main {
-  test() {
-    return true;
+  calculate(scoresheet) {
+    var frameScores;
+
+    frameScores = this.calculateFrameScores(List().merge(scoresheet.split('')));
+    return this.addUpFrameScores(frameScores);
+  }
+
+  calculateFrameScores(frames) {
+    var frameScores, frame;
+
+    frame = 1;
+    frameScores = List();
+    while(frame <= NUM_FRAMES) {
+      frameScores = frameScores.push(this.getFrameScore(frames));
+      frames = this.nextFrame(frames);
+      frame++;
+    }
+
+    return frameScores;
+  }
+
+  addUpFrameScores(frameScores) {
+    return frameScores.reduce((r, n) => {
+      return r + n;
+    });
+  }
+
+  nextFrame(frames) {
+    if (this.isStrike(frames.first())) {
+      return this.removeRoll(frames);
+    } else {
+      frames = this.removeRoll(frames);
+      return this.removeRoll(frames);
+    }
+  }
+
+  removeRoll(frames) {
+    return frames.shift();
+  }
+
+  getFrameScore(frames) {
+    let firstRoll = frames.first();
+    this.checkForValidRoll(firstRoll);
+
+    if (this.isStrike(firstRoll)) {
+      return this.strikeScore(frames);
+    } else if (this.isSpare(this.nextRoll(frames))) {
+      return this.spareScore(this.removeRoll(frames));
+    } else {
+      let secondRoll = this.nextRoll(frames);
+      this.checkForValidRoll(secondRoll);
+      return this.getRawFrameScore(frames);
+    }
+  }
+
+  checkForValidRoll(roll) {
+    if (!this.isStrike(roll) && !this.isNormalRoll(roll)) {
+      throw new Error('Invalid Roll');
+    }
+  }
+
+  strikeScore(frames) {
+    frames = this.removeRoll(frames);
+    return NUM_PINS + this.getRawFrameScore(frames);
+  }
+
+  spareScore(frames) {
+    return NUM_PINS + this.getRawRollScore(this.nextRoll(frames));
+  }
+
+  nextRoll(frames) {
+    return this.removeRoll(frames).first();
+  }
+
+  getRawFrameScore(frames) {
+    var firstRollScore, secondRollScore;
+
+    firstRollScore = this.getRawRollScore(frames.first());
+    secondRollScore = this.getRawRollScore(this.nextRoll(frames), firstRollScore);
+
+    return firstRollScore + secondRollScore;
+  }
+
+  getRawRollScore(roll, firstRollScore = 0) {
+    switch (roll) {
+      case 'x':
+        return NUM_PINS;
+      case '-':
+        return 0;
+      case '/':
+        return NUM_PINS - firstRollScore;
+      default:
+        return this.toNumber(roll);
+    }
+  }
+
+  isStrike(roll) {
+    return roll === 'x';
+  }
+
+  isSpare(roll) {
+    return roll === '/';
+  }
+
+  isNormalRoll(roll) {
+    return this.isANumber(roll) || this.isGutter(roll);
+  }
+
+  isGutter(roll) {
+    return roll === '-';
+  }
+
+  isANumber(roll) {
+    return !isNaN(this.toNumber(roll));
+  }
+
+
+  toNumber(roll) {
+    return parseInt(roll, 10);
   }
 }
 
--- test/main_spec.js
+++ test/main_spec.js
@@ -1,11 +1,89 @@
 import { Main } from '../transpiled/main';
 
 describe('Main', () => {
-  it('passes a test', () => {
-    var main;
+  var main;
 
+  beforeEach(() => {
     main = new Main();
+  });
 
-    expect(main.test()).toBe(true);
+  it('calculates a basic score', () => {
+    expect(main.calculate('12345123451234512345')).toBe(60);
+  });
+
+  it('calculates gutter balls', () => {
+    expect(main.calculate('1-3451234512345-2345')).toBe(57);
+  });
+
+  it('calculates a heartbreak', () => {
+    expect(main.calculate('9-9-9-9-9-9-9-9-9-9-')).toBe(90);
+  });
+
+  it('calculates a spare', () => {
+    expect(main.calculate('1/345123451234512345')).toBe(70);
+  });
+
+  it('calculates multiple spares', () => {
+    expect(main.calculate('1/345/23451234512345')).toBe(76);
+  });
+
+  it('calculates consecutive spares', () => {
+    expect(main.calculate('1/3/5/23451234512345')).toBe(84);
+  });
+
+  it('calculates a spare on every roll', () => {
+    expect(main.calculate('5/5/5/5/5/5/5/5/5/5/5')).toBe(150);
+  });
+
+  it('calculates a strike', () => {
+    expect(main.calculate('x345123451234512345')).toBe(74);
+  });
+
+  it('calculates consecutive strikes', () => {
+    expect(main.calculate('xx5123451234512345')).toBe(91);
+  });
+
+  it('calculates a perfect game', () => {
+    expect(main.calculate('xxxxxxxxxxxx')).toBe(300);
+  });
+
+  it('calculates a mixture of spares, gutters, and strikes', () => {
+    expect(main.calculate('1/3/5/-3x1234x2/x9/')).toBe(124);
+  });
+
+  it('calculates an ending turkey', () => {
+    expect(main.calculate('1/3/5/-3x1234x2/xxx')).toBe(134);
+  });
+
+  it('calculates another mixture of spares and strikes', () => {
+    expect(main.calculate('1/3/5/-3x1/xx2/xxx')).toBe(173);
+  });
+
+  it('calculates a spare and strik on the last frame', () => {
+    expect(main.calculate('x9/9/9/9/9/9/9/9/9/x')).toBe(192);
+  });
+
+  it('calculates all gutter balls', () => {
+    expect(main.calculate('--------------------')).toBe(0);
+  });
+
+  it('throws an error when the first roll is a spare', () => {
+    expect(() => { main.calculate('11//9/9/9/9/9/9/9/9/x'); }).toThrow(new Error('Invalid Roll'));
+  });
+
+  it('throws an error when the first roll after a strike is a spare', () => {
+    expect(() => { main.calculate('x//9/9/9/9/9/9/9/9/x'); }).toThrow(new Error('Invalid Roll'));
+  });
+
+  it('throws an error when the first roll is not a number, strike, spare, or gutter', () => {
+    expect(() => { main.calculate('xa/9/9/9/9/9/9/9/9/x'); }).toThrow(new Error('Invalid Roll'));
+  });
+
+  it('throws an error when the first roll is not a number, strike, spare, or gutter', () => {
+    expect(() => { main.calculate('ax/9/9/9/9/9/9/9/9/x'); }).toThrow(new Error('Invalid Roll'));
+  });
+
+  it('throws an error when the roll after a spare is invalid', () => {
+    expect(() => { main.calculate('119/a/9/9/9/9/9/9/9/x'); }).toThrow(new Error('Invalid Roll'));
   });
 });
